7875a8e61abec7c1e45983b9f47a6eb2746505eb
Pankei <studypankeev@gmail.com>
2022-12-04 20:35:23 +0400

    Initial Commit

---
+++ labaisd2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  labaisd2
+//
+//  Created by Глеб Панкеев on 04.12.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
