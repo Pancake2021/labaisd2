895ec3c012a55047e2aa26c961ce5490a90a3ff7
Pankei <studypankeev@gmail.com>
2022-12-04 20:36:51 +0400

    creating a new project

--- labaisd2/main.cpp
+++ labaisd2/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  labaisd2
-//
-//  Created by Глеб Панкеев on 04.12.2022.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
